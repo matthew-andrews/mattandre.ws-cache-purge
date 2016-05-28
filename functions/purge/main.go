22452db424752a5c3cf14c172be6c6ce1e48b274
Matthew Andrews <matt@mattandre.ws>
2016-05-28 14:50:19 +0100

    Pass the output through

--- functions/purge/main.go
+++ functions/purge/main.go
@@ -6,34 +6,42 @@ import (
 	"errors"
 	"fmt"
 	"github.com/apex/go-apex"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
 
-type message struct {
+type Message struct {
 	Urls []string `json:"urls"`
 }
 
-type PurgeCacheRequest struct {
+type PurgeCacheRequestBody struct {
 	Files []string `json:"files"`
+}
+
+type PurgeCacheResponseBody struct {
+	Result struct {
+		Id string `json:"id"`
+	} `json:"result"`
+	Success bool `json:"success"`
 }
 
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-		var m message
+		var m Message
 
 		if err := json.Unmarshal(event, &m); err != nil {
 			return nil, err
 		}
 
-		requestObject, err := json.Marshal(PurgeCacheRequest{
+		requestBody, err := json.Marshal(PurgeCacheRequestBody{
 			Files: m.Urls,
 		})
 		if err != nil {
 			return nil, err
 		}
 
-		body := bytes.NewBuffer(requestObject)
+		body := bytes.NewBuffer(requestBody)
 
 		client := &http.Client{}
 		url := "https://api.cloudflare.com/client/v4/zones/" + os.Getenv("CLOUDFLARE_IDENTIFIER") + "/purge_cache"
@@ -55,6 +63,16 @@ func main() {
 		if resp.StatusCode > 399 {
 			return nil, errors.New("Bad Server Response: " + resp.Status)
 		}
-		return m, nil
+
+		bytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		var responseBody PurgeCacheResponseBody
+		if err := json.Unmarshal(bytes, &responseBody); err != nil {
+			return nil, err
+		}
+
+		return responseBody, nil
 	})
 }
