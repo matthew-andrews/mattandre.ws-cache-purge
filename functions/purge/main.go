60dbea6bf9113f8732ba3d5a890b117991e1ae75
Matthew Andrews <matt@mattandre.ws>
2016-05-28 13:47:52 +0100

    Put back to default go apex example

--- functions/purge/main.go
+++ functions/purge/main.go
@@ -2,24 +2,21 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/apex/go-apex"
-	"time"
 )
 
 type message struct {
-	China string `json:"china"`
+	Hello string `json:"hello"`
 }
 
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-		london, _ := time.LoadLocation("Europe/London")
-		china := time.Date(2016, 8, 9, 0, 0, 0, 0, london)
-		now := time.Now()
-		totalDays := int(china.Sub(now).Hours() / 24)
-		weeks := (totalDays - totalDays%7) / 7
-		days := totalDays % 7
-		output := fmt.Sprintf("%d weeks, %d days", weeks, days)
-		return message{output}, nil
+		var m message
+
+		if err := json.Unmarshal(event, &m); err != nil {
+			return nil, err
+		}
+
+		return m, nil
 	})
 }
