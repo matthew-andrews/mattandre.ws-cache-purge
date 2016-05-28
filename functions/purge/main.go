8c37d4335ff9fe8c63d6198b139ee9f73194adef
Matthew Andrews <matt@mattandre.ws>
2016-05-28 14:14:56 +0100

    Stub something out

--- functions/purge/main.go
+++ functions/purge/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/apex/go-apex"
+	"net/http"
+	"os"
+	"strings"
 )
 
 type message struct {
-	Hello string `json:"hello"`
+	Url string `json:"url"`
 }
 
 func main() {
@@ -17,6 +21,23 @@ func main() {
 			return nil, err
 		}
 
+		body := strings.NewReader("{\"files\":[\"" + m.Url + "/\"]}")
+
+		client := &http.Client{}
+		if req, err := http.NewRequest("DELETE", "https://api.cloudflare.com/client/v4/"+os.Getenv("CLOUDFLARE_IDENTIFIER")+"/purge_cache", body); err != nil {
+			return nil, err
+		}
+
+		req.Header.Add("X-Auth-Email", "matt@mattandre.ws")
+		req.Header.Add("X-Auth-Key", os.Getenv("CLOUDFLARE_API_KEY"))
+		req.Header.Add("Content-Type", "application/json")
+
+		if resp, err := client.Do(req); err != nil {
+			return nil, err
+		}
+		if resp.StatusCode > 399 {
+			return errors.New("Bad Server Response: " + resp.Status)
+		}
 		return m, nil
 	})
 }
